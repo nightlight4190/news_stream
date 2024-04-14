715a6472d0e49a945c53b1283830d3f1dcd8afa4
yogendra0914 <yogensubedi1855@gmail.com>
2024-04-14 21:27:38 +0545

    final commit

--- lib/view/screens/widgets/newsContainer.dart
+++ lib/view/screens/widgets/newsContainer.dart
@@ -1,82 +1,74 @@
 import 'package:flutter/material.dart';
-import 'package:news_stream/view/screens/detail.dart';
 
+import '../detail.dart';
+
+// ignore: must_be_immutable
 class NewsContainer extends StatelessWidget {
   String imgUrl;
   String newsHead;
-  String newsDesc;
-  String newsCnt;
+  String newsDes;
   String newsUrl;
+  String newsCnt;
   NewsContainer(
       {super.key,
       required this.imgUrl,
+      required this.newsDes,
+      required this.newsCnt,
       required this.newsHead,
-      required this.newsDesc,
-      required this.newsUrl,
-      required this.newsCnt});
+      required this.newsUrl});
 
   @override
   Widget build(BuildContext context) {
     return Container(
       height: MediaQuery.of(context).size.height,
       width: MediaQuery.of(context).size.width,
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Image.network(
-            imgUrl,
-            height: 350,
+      child: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+        FadeInImage.assetNetwork(
+            height: 250,
             width: MediaQuery.of(context).size.width,
             fit: BoxFit.cover,
-          ),
-          Container(
-            padding: EdgeInsets.symmetric(horizontal: 20),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                SizedBox(
-                  height: 10,
-                ),
-                Text(
-                  newsHead.length > 60
-                      ? "${newsHead.substring(0, 60)}"
-                      : newsHead,
-                  style: TextStyle(
-                    fontSize: 20,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-                SizedBox(
-                  height: 10,
-                ),
-                Text(
-                  newsDesc,
-                  style: TextStyle(
-                    fontSize: 12,
-                    color: Colors.black54,
-                  ),
-                ),
-                SizedBox(
-                  height: 10,
-                ),
-                Text(
-                  newsCnt.length > 200
-                      ? newsCnt.substring(0, 300)
-                      : "${newsCnt.toString().substring(0, newsCnt.length - 15)}...",
-                  style: TextStyle(
-                    fontSize: 16,
-                  ),
-                ),
-              ],
+            placeholder: "assets/image/breakingNews.jpg",
+            image: imgUrl),
+        Container(
+          padding: EdgeInsets.symmetric(horizontal: 20),
+          child:
+              Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+            SizedBox(
+              height: 30,
             ),
-          ),
-          Spacer(),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 20.0),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: [
-                ElevatedButton(
+            Text(
+              newsHead.length > 90
+                  ? "${newsHead.substring(0, 90)}..."
+                  : newsHead,
+              style: TextStyle(fontSize: 23, fontWeight: FontWeight.bold),
+            ),
+            SizedBox(
+              height: 30,
+            ),
+            Text(
+              newsDes,
+              style: TextStyle(fontSize: 12, color: Colors.black38),
+            ),
+            SizedBox(
+              height: 30,
+            ),
+            Text(
+              newsCnt != "--"
+                  ? newsCnt.length > 250
+                      ? newsCnt.substring(0, 250)
+                      : "${newsCnt.toString().substring(0, newsCnt.length - 15)}..."
+                  : newsCnt,
+              style: TextStyle(fontSize: 16),
+            ),
+          ]),
+        ),
+        Spacer(),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20),
+              child: ElevatedButton(
                   onPressed: () {
                     Navigator.push(
                         context,
@@ -84,21 +76,14 @@ class NewsContainer extends StatelessWidget {
                             builder: (context) =>
                                 DetailScreen(newsUrl: newsUrl)));
                   },
-                  child: Text(
-                    'Read More',
-                    style: TextStyle(
-                      color: Colors.teal,
-                    ),
-                  ),
-                ),
-              ],
+                  child: Text("Read More")),
             ),
-          ),
-          SizedBox(
-            height: 20,
-          ),
-        ],
-      ),
+          ],
+        ),
+        SizedBox(
+          height: 20,
+        ),
+      ]),
     );
   }
 }
