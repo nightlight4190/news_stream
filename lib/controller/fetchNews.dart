c8632f65c6a243ce3ebd26b117e44ab047ccd9b7
yogendra0914 <yogensubedi1855@gmail.com>
2024-04-14 21:16:11 +0545

    final commit

--- lib/controller/fetchNews.dart
+++ lib/controller/fetchNews.dart
@@ -42,7 +42,8 @@ class FetchNews {
         "https://newsapi.org/v2/top-headlines?sources=$sourceID&apiKey=affb4a3daf554ce9b41e31889811d2ef"));
 
     Map body_data = jsonDecode(response.body);
-    Map articles = body_data["articles"];
+    List articles = body_data["articles"];
+    final Random() = new Random();
 
     var myArticle = articles[_random.nextInt(articles.length)];
 
--- lib/view/screens/home.dart
+++ lib/view/screens/home.dart
@@ -72,7 +72,7 @@ class _HomeScreenState extends State<HomeScreen> {
                   imgUrl: newsArt.imgUrl,
                   newsCnt: newsArt.newsCnt,
                   newsHead: newsArt.newsHead,
-                  newsDesc: newsArt.newsDesc,
+                  newsDes: newsArt.newsDesc,
                   newsUrl: newsArt.newsUrl);
         },
       ),
